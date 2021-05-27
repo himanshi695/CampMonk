1ebba1b277d9b3eb9c50df90a11675ad385585a3
himanshi695 <himanshi.agrawal_cs18@gla.ac.in>
2021-05-27 12:07:44 +0530

    Adding Images

--- models/campground.js
+++ models/campground.js
@@ -3,8 +3,9 @@ const Schema  = mongoose.Schema;
 
 const CampgroundSchema = new Schema({
     title:String,
-    price:String, 
-    description:String,
+    image:String,
+    price:Number, 
+    description : String,
     location : String
 });
 
