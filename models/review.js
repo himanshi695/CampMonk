4bf2b00815f8e9605139588525f4d9ffc873ff04
himanshi695 <himanshi.agrawal_cs18@gla.ac.in>
2021-05-29 12:45:30 +0530

    Defined Basic Review Model

---
+++ models/review.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+const { campgroundSchema } = require('../schemas');
+const Schema = mongoose.Schema;
+
+const reviewSchema = new Schema ({
+    body: String,
+    rating: Number
+});
+
+module.exports = mongoose.model('Review', reviewSchema);
