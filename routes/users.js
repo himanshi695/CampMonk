4b4c2f61d2d59508235b2c7c713d21c18e72573c
himanshi695 <himanshi.agrawal_cs18@gla.ac.in>
2021-06-04 11:01:41 +0530

    Added Login Routes

--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,34 @@
 const express = require('express');
+const passport = require('passport');
 const router =  express.Router();
+const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 
 router.get('/register', (req,res)=>{
     res.render('users/register')
 })
 
-router.post('/register', async(req,res)=>{
-    res.send(req.body);
+router.post('/register', catchAsync( async(req,res)=>{
+    try{
+    const { email , username, password} = req.body;
+    const user = new User({email, username});
+    const registeredUser = await User.register(user, password);
+    //console.log(registeredUser);
+    req.flash('success','Welcome to Camp Monk!');
+    res.redirect('/campgrounds');
+    } catch (e){
+        req.flash('error', e.message);
+        res.redirect('/register');
+    }
+}))
+
+router.get('/login', (req,res)=>{
+    res.render('users/login');
+})
+
+router.post('/login', passport.authenticate('local',{failureFlash:true, failureRedirect:'/login'}),(req,res)=>{
+    req.flash('success', 'welcome back!');
+    res.redirect('/campgrounds');
 })
 
 module.exports = router;
