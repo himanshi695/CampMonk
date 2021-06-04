55882157b58c5b1136edf47448d424fe8349c088
himanshi695 <himanshi.agrawal_cs18@gla.ac.in>
2021-06-04 12:35:23 +0530

    Fixed Register Route

--- routes/users.js
+++ routes/users.js
@@ -8,14 +8,17 @@ router.get('/register', (req,res)=>{
     res.render('users/register')
 })
 
-router.post('/register', catchAsync( async(req,res)=>{
+router.post('/register', catchAsync( async(req,res,next)=>{
     try{
     const { email , username, password} = req.body;
     const user = new User({email, username});
     const registeredUser = await User.register(user, password);
+    req.login(registeredUser, err=>{
+        if(err) return next(err);
+        req.flash('success','Welcome to Camp Monk!');
+        res.redirect('/campgrounds');
+    })
     //console.log(registeredUser);
-    req.flash('success','Welcome to Camp Monk!');
-    res.redirect('/campgrounds');
     } catch (e){
         req.flash('error', e.message);
         res.redirect('/register');
@@ -28,7 +31,17 @@ router.get('/login', (req,res)=>{
 
 router.post('/login', passport.authenticate('local',{failureFlash:true, failureRedirect:'/login'}),(req,res)=>{
     req.flash('success', 'welcome back!');
+    const redirectUrl = req.session.returnTo || '/campgrounds';
+    delete req.session.returnTo;
+    res.redirect(redirectUrl);
+})
+
+router.get('/logout', (req,res)=>{
+    req.logout();
+    req.flash('success', 'Goodbye!');
     res.redirect('/campgrounds');
 })
+
+
 
 module.exports = router;
