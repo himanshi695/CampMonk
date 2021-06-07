39f629d8dfa98837f42924a13ea231df26a6d21d
himanshi695 <himanshi.agrawal_cs18@gla.ac.in>
2021-06-07 14:26:17 +0530

    Routes Updated!

--- routes/users.js
+++ routes/users.js
@@ -5,13 +5,15 @@ const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
 const users = require('../controllers/users');
 
-router.get('/register', users.renderRegister)
+router.route('/register')
 
-router.post('/register', catchAsync( users.register));
+    .get(users.renderRegister)
+    .post(catchAsync( users.register));
 
-router.get('/login', users.renderLogin);
+router.route('/login')
 
-router.post('/login', passport.authenticate('local',{failureFlash:true, failureRedirect:'/login'}),users.login);
+    .get( users.renderLogin)
+    .post( passport.authenticate('local',{failureFlash:true, failureRedirect:'/login'}),users.login);
 
 router.get('/logout',users.logout)
 
