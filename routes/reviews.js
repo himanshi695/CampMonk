caf5598330acf066b66dccc0d07fc99875aa92af
himanshi695 <himanshi.agrawal_cs18@gla.ac.in>
2021-06-07 12:58:21 +0530

    Added Reviews Permission

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,16 +1,17 @@
 const express= require('express');
 const router = express.Router({mergeParams : true});
 const catchAsync = require('../utils/catchAsync');
-const { validateReview } = require('../middleware');
+const { validateReview, isLoggedIn } = require('../middleware');
 const ExpressError = require('../utils/ExpressError');
 const Campground = require('../models/campground');
 const Review = require('../models/review');
 
 
 
-router.post('/', validateReview, catchAsync(async( req, res)=>{
+router.post('/', isLoggedIn, validateReview, catchAsync(async( req, res)=>{
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
+    review.author = req.user._id;
     campground.reviews.push(review);
     await review.save();
     await campground.save();
