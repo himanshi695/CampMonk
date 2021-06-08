01b19914ca2ec4f6ef8577b1962d2d77cb7b22d9
himanshi695 <himanshi.agrawal_cs18@gla.ac.in>
2021-06-08 14:40:10 +0530

    Added Image Upload

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,13 +4,17 @@ const campgrounds = require('../controllers/campgrounds');
 const catchAsync = require('../utils/catchAsync');
 
 const {isLoggedIn, isAuthor, validateCampground} = require('../middleware');
+const multer= require('multer');
+const {storage} = require('../cloudinary');
+const upload = multer({ storage })
 
 const Campground = require('../models/campground');
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground));
-
+    .post(isLoggedIn,
+        upload.array('image'), validateCampground, catchAsync(campgrounds.createCampground));
+    
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
 router.route('/:id')
