69e74c8e3d0ccf5670441cadce073269c1804540
himanshi695 <himanshi.agrawal_cs18@gla.ac.in>
2021-08-21 02:41:15 +0530

    Refactored

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ const MongoDBStore = require('connect-mongo');
 //Linking the database below when in production
 //'mongodb://localhost:27017/camp-monk' 
 //linking the database here with the one on Heroku with dbUrl =process.env.DB_URL
-const dbUrl = 'mongodb://localhost:27017/camp-monk' || process.env.DB_URL ;
+const dbUrl =  process.env.DB_URL || 'mongodb://localhost:27017/camp-monk' ;
 
 // URL to use for the depolyed version on Heroku
 mongoose.connect(dbUrl , {
