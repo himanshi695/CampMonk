b6f04187fefe45da5a35cbee44e62e477b1d8355
himanshi695 <himanshi.agrawal_cs18@gla.ac.in>
2021-08-21 01:20:11 +0530

    Refactored the code

--- models/user.js
+++ models/user.js
@@ -2,6 +2,7 @@ const mongoose=require('mongoose');
 const Schema = mongoose.Schema;
 const passportLocalMongoose = require('passport-local-mongoose');
 
+//User schema setup
 const UserSchema = new Schema({
     email: {
         type:String,
@@ -12,4 +13,5 @@ const UserSchema = new Schema({
 
 UserSchema.plugin(passportLocalMongoose);
 
+//Compiling schema into model
 module.exports = mongoose.model('User', UserSchema);
